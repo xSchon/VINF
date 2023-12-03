84df55ff6e50a475d9fa6faccfd3a3bb0c2c4f96
Martin Schön <martin.schon@softpoint.sk>
2023-12-03 03:33:28 +0100

    Comment and format with the usage of black

--- searcher.py
+++ searcher.py
@@ -10,66 +10,86 @@ from org.apache.lucene.store import FSDirectory
 from java.nio.file import Paths
 
 lucene.initVM()
-# Change paths accordingly
+# Change paths accordingly, as needed
 FILE_PATH = os.getcwd()
 INDEX_DIR = f"{FILE_PATH}/players_index"
 
+
 class Searcher:
-    def __init__(self, index_dir = INDEX_DIR) -> None:
+    """Create class for searching in index created over players, in indexer.py"""
+
+    def __init__(self, index_dir=INDEX_DIR) -> None:
         self.index_dir = index_dir
 
-    def search_index(self, fields_to_search:list, queries:list, printable_cols:list = ['Team', 'PlayerName'], results_amount:str = '5', search_type:str = 'OR'):
-        # Create an IndexSearcher
+    def search_index(
+        self,
+        fields_to_search: list,
+        queries: list,
+        printable_cols: list = ["Team", "PlayerName"],
+        results_amount: str = "5",
+        search_type: str = "OR",
+    ) -> list:
+        """
+        Main searching class
+        args:
+            :list fields_to_search - which columns to search in
+            :list queries - representing words to search in columns on same positions in fields_to_search
+            :list printable_cols - data from which columns to show to the end user
+            :str results_amount - how many results to show. Default 5, can be int in string, or 'All' for all results
+            :str search_type - how to access different columns and searches. Deafult OR, can be OR or AND
+
+        returns:
+            list containing prepared strings of information filtered accordingly to the query
+        """
+
         # Create an IndexSearcher
         directory = FSDirectory.open(Paths.get(self.index_dir))
         reader = DirectoryReader.open(directory)
         searcher = IndexSearcher(reader)
 
-        # Build a boolean query for each field
+        # Build a query over multiple columns
         boolean_query = BooleanQuery.Builder()
-
         for field, query_text in zip(fields_to_search, queries):
             query_parser = QueryParser(field, StandardAnalyzer())
             field_query = query_parser.parse(query_text)
             # OR query
-            if search_type == 'OR':
+            if search_type == "OR":
                 boolean_query.add(field_query, BooleanClause.Occur.SHOULD)
             # AND query
             else:
                 boolean_query.add(field_query, BooleanClause.Occur.MUST)
 
+        # Filter
         final_query = boolean_query.build()
-
         top_docs = searcher.search(final_query, reader.maxDoc())
 
         # Print the total number of results found from the dataset
         total_results = top_docs.totalHits
         print(f"Total results: {total_results}")
 
-        # Iterate through all results and select fitting
+        # Iterate through all results and prepare string selecting fitting
         results = []
-        
-        # Change the amount of results retreived
-        if results_amount == 'All':
-            results_amount =  reader.maxDoc() # maximum results        
+        if results_amount == "All":
+            results_amount = reader.maxDoc()  # maximum results
         results_amount = int(results_amount)
 
-        for score_doc in sorted(top_docs.scoreDocs, key=lambda x: x.score, reverse=True):
+        for score_doc in sorted(
+            top_docs.scoreDocs, key=lambda x: x.score, reverse=True
+        ):
             doc_id = score_doc.doc
             relevance_score = score_doc.score
-            
-            # Access the fields of the document using the IndexSearcher
+
             document = searcher.doc(doc_id)
             printable_string = ""
             for toprint in printable_cols:
                 printable_string += f"{toprint}: {document.get(toprint)}  "
-                
+
+            # Select unique
             if printable_string not in results:
                 results.append(printable_string)
                 results_amount -= 1
 
             if results_amount <= 0:
                 break
-        
+
         return results
-            
